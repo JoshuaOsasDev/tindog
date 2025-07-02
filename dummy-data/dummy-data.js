25d6e1e7892ef70f1ce8fdb5152814bdff230ad1
JoshuaOsasDev <ebighoselejoshua@gmail.com>
2025-07-02 03:24:04 -0700

    dummy data

--- dummy-data/dummy-data.js
+++ dummy-data/dummy-data.js
@@ -88,3 +88,41 @@ export const dogs = [
     adopt: false,
   },
 ];
+
+const blogPosts = [
+  {
+    title: "5 Tips to Prepare Your Home for a New Dog",
+    excerpt:
+      "Bringing home a new furry friend? Here's how to make them feel welcome and safe.",
+    image: "/coco.jpg",
+    date: "Novermber 15, 2025",
+  },
+  {
+    title: "Why Rescue Dogs Make the Best Pets",
+    excerpt:
+      "Adopting a rescue dog isn't just kind—it’s incredibly rewarding. Here's why.",
+    image: "/Bella.jpg",
+    date: "June 10, 2025",
+  },
+  {
+    title: "Healthy Treats Your Dog Will Love",
+    excerpt:
+      "Ditch the store-bought snacks. Try these vet-approved homemade treats.",
+    image: "/charlie.jpg",
+    date: "May 28, 2025",
+  },
+  {
+    title: "Why Rescue Dogs Make the Best Pets",
+    excerpt:
+      "Adopting a rescue dog isn't just kind—it’s incredibly rewarding. Here's why.",
+    image: "/Bella.jpg",
+    date: "June 10, 2025",
+  },
+  {
+    title: "Healthy Treats Your Dog Will Love",
+    excerpt:
+      "Ditch the store-bought snacks. Try these vet-approved homemade treats.",
+    image: "/charlie.jpg",
+    date: "May 28, 2025",
+  },
+];
--- src/Components/BlogSection.jsx
+++ src/Components/BlogSection.jsx
@@ -5,43 +5,6 @@ import { useGetblogPost } from "./useFecthData";
 function BlogSection() {
   const { posts: blogPosts } = useGetblogPost();
   console.log(blogPosts, "post");
-  // const blogPosts = [
-  //   {
-  //     title: "5 Tips to Prepare Your Home for a New Dog",
-  //     excerpt:
-  //       "Bringing home a new furry friend? Here's how to make them feel welcome and safe.",
-  //     image: "/coco.jpg",
-  //     date: "Novermber 15, 2025",
-  //   },
-  //   {
-  //     title: "Why Rescue Dogs Make the Best Pets",
-  //     excerpt:
-  //       "Adopting a rescue dog isn't just kind—it’s incredibly rewarding. Here's why.",
-  //     image: "/Bella.jpg",
-  //     date: "June 10, 2025",
-  //   },
-  //   {
-  //     title: "Healthy Treats Your Dog Will Love",
-  //     excerpt:
-  //       "Ditch the store-bought snacks. Try these vet-approved homemade treats.",
-  //     image: "/charlie.jpg",
-  //     date: "May 28, 2025",
-  //   },
-  //   {
-  //     title: "Why Rescue Dogs Make the Best Pets",
-  //     excerpt:
-  //       "Adopting a rescue dog isn't just kind—it’s incredibly rewarding. Here's why.",
-  //     image: "/Bella.jpg",
-  //     date: "June 10, 2025",
-  //   },
-  //   {
-  //     title: "Healthy Treats Your Dog Will Love",
-  //     excerpt:
-  //       "Ditch the store-bought snacks. Try these vet-approved homemade treats.",
-  //     image: "/charlie.jpg",
-  //     date: "May 28, 2025",
-  //   },
-  // ];
 
   return (
     <section className="m-auto py-20">
